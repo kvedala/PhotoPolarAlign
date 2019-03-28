f28f17287d1a9c4854373cdc3de8d29fd099b804
Krishna Vedala <krishna.vedala@ieee.org>
2019-03-28 18:24:21 -0400

    cleanup and add options to main app window

--- main.js
+++ main.js
@@ -1,21 +1,27 @@
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
+const { Menu, MenuItem } = require('electron')
 
-let win
+let mainWin
 
 function createWindow() {
-    win = new BrowserWindow({ width: 800, height: 600})
+    mainWin = new BrowserWindow({
+        width: 800, height: 600, resizable: false,
+        maximizable: false, fullscreenable: false,
+        icon: 'images/PPA.ico',
+        darkTheme: true
+    })
 
-    win.loadFile('index.html')
-    win.webContents.openDevTools()
+    mainWin.loadFile('index.html')
+    mainWin.webContents.openDevTools()
 
-    // win.on('closed', () => {
-    //     win=null
-    // })
+    mainWin.on('closed', () => {
+        mainWin = null
+    })
 }
 
 app.on('ready', createWindow)
-// app.on('window-all-closed', () => {
-//     if (process.platform !== 'darwin') {
-//         app.quit()
-//     }
-// })
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
