6fbc0a2fd4bc35b0caa0e60cc5bf8cf5ed7e2b6c
Krishna Vedala <krishna.vedala@ieee.org>
2019-03-28 19:19:49 -0400

    add settings package

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const { app, BrowserWindow, ipcMain } = require('electron')
 const { Menu, MenuItem } = require('electron')
+const settings = require('electron-settings')
 
 let mainWin
 
 function createWindow() {
+    app.requestSingleInstanceLock()
     mainWin = new BrowserWindow({
         width: 800, height: 600, resizable: false,
         maximizable: false, fullscreenable: false,
