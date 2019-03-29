6466c57d88b29dfed9d9ececd138ed7f200ae0ed
Krishna Vedala <krishna.vedala@ieee.org>
2019-03-28 23:59:13 -0400

    check API for astrometry.net

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,5 +1,21 @@
-let $ = require('jquery')
+let $ = require('jquery');
 
-$('#btnAPI').on('click', () => {
-    $('#preferences').children().prop('disabled', true)
+$('#btnAPI').on('click', function (event) {
+    const jsonKey = { 'apikey': document.getElementById("txtApiKey").value }
+    const keyStr = JSON.stringify(jsonKey)
+    console.log('request-json=' + keyStr)
+    // $('#preferences').children().prop('disabled', true);
+    $.ajax({
+        type: "POST",
+        url: "http://nova.astrometry.net/api/login",
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        contentType: 'application/x-www-form-urlencoded; charset=utf-8',
+        data: 'request-json=' + keyStr,
+        // success: function (data) {
+        //     console.log(data)
+        // },
+        complete: function (xhr, status) {
+            console.log(status + ': ' + xhr)
+        }
+    })
 })
